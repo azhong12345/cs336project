d5a0859372f67e78290fc704b0166f3700c97924
azhong12345 <57428595+azhong12345@users.noreply.github.com>
2021-08-12 11:25:45 -0400

    updated schema

--- cs336project.sql
+++ cs336project.sql
@@ -16,6 +16,297 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `alerts`
+--
+
+DROP TABLE IF EXISTS `alerts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alerts` (
+  `username` varchar(30) NOT NULL,
+  `itemID` int NOT NULL,
+  `isAvailable` int DEFAULT NULL,
+  PRIMARY KEY (`username`,`itemID`),
+  KEY `itemID` (`itemID`),
+  CONSTRAINT `alerts_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  CONSTRAINT `alerts_ibfk_2` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alerts`
+--
+
+LOCK TABLES `alerts` WRITE;
+/*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auction`
+--
+
+DROP TABLE IF EXISTS `auction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auction` (
+  `auctionID` int NOT NULL AUTO_INCREMENT,
+  `itemID` int DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `open_date` datetime DEFAULT NULL,
+  `close_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`auctionID`),
+  KEY `itemID` (`itemID`),
+  KEY `username` (`username`),
+  CONSTRAINT `auction_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`),
+  CONSTRAINT `auction_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auction`
+--
+
+LOCK TABLES `auction` WRITE;
+/*!40000 ALTER TABLE `auction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auto_bid`
+--
+
+DROP TABLE IF EXISTS `auto_bid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auto_bid` (
+  `username` varchar(30) NOT NULL,
+  `auctionID` int NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `is_anon` int DEFAULT NULL,
+  `upper_limit` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`username`,`auctionID`,`upper_limit`),
+  KEY `auctionID` (`auctionID`),
+  CONSTRAINT `auto_bid_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  CONSTRAINT `auto_bid_ibfk_2` FOREIGN KEY (`auctionID`) REFERENCES `auction` (`auctionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auto_bid`
+--
+
+LOCK TABLES `auto_bid` WRITE;
+/*!40000 ALTER TABLE `auto_bid` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auto_bid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bid`
+--
+
+DROP TABLE IF EXISTS `bid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bid` (
+  `username` varchar(30) NOT NULL,
+  `auctionID` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `is_anon` int DEFAULT NULL,
+  PRIMARY KEY (`username`,`auctionID`,`price`),
+  KEY `auctionID` (`auctionID`),
+  CONSTRAINT `bid_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  CONSTRAINT `bid_ibfk_2` FOREIGN KEY (`auctionID`) REFERENCES `auction` (`auctionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bid`
+--
+
+LOCK TABLES `bid` WRITE;
+/*!40000 ALTER TABLE `bid` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clothing`
+--
+
+DROP TABLE IF EXISTS `clothing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clothing` (
+  `itemID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `brand` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`itemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clothing`
+--
+
+LOCK TABLES `clothing` WRITE;
+/*!40000 ALTER TABLE `clothing` DISABLE KEYS */;
+INSERT INTO `clothing` VALUES (1,'summer dress',NULL,'gap'),(2,'dress',NULL,'gap'),(4,'jeans','men','levis');
+/*!40000 ALTER TABLE `clothing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_service`
+--
+
+DROP TABLE IF EXISTS `customer_service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_service` (
+  `ticketID` int NOT NULL AUTO_INCREMENT,
+  `end_username` varchar(30) DEFAULT NULL,
+  `rep_username` varchar(30) DEFAULT NULL,
+  `question` varchar(500) DEFAULT NULL,
+  `answer` varchar(700) DEFAULT NULL,
+  PRIMARY KEY (`ticketID`),
+  KEY `end_username` (`end_username`),
+  KEY `rep_username` (`rep_username`),
+  CONSTRAINT `customer_service_ibfk_1` FOREIGN KEY (`end_username`) REFERENCES `user` (`username`),
+  CONSTRAINT `customer_service_ibfk_2` FOREIGN KEY (`rep_username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_service`
+--
+
+LOCK TABLES `customer_service` WRITE;
+/*!40000 ALTER TABLE `customer_service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dress`
+--
+
+DROP TABLE IF EXISTS `dress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dress` (
+  `itemID` int NOT NULL,
+  `dress_size` int DEFAULT NULL,
+  `dress_length` int DEFAULT NULL,
+  PRIMARY KEY (`itemID`),
+  CONSTRAINT `dress_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dress`
+--
+
+LOCK TABLES `dress` WRITE;
+/*!40000 ALTER TABLE `dress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `end_view`
+--
+
+DROP TABLE IF EXISTS `end_view`;
+/*!50001 DROP VIEW IF EXISTS `end_view`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `end_view` AS SELECT 
+ 1 AS `name`,
+ 1 AS `gender`,
+ 1 AS `brand`,
+ 1 AS `username`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `pants`
+--
+
+DROP TABLE IF EXISTS `pants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pants` (
+  `itemID` int NOT NULL,
+  `pants_size` int DEFAULT NULL,
+  PRIMARY KEY (`itemID`),
+  CONSTRAINT `pants_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pants`
+--
+
+LOCK TABLES `pants` WRITE;
+/*!40000 ALTER TABLE `pants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sell`
+--
+
+DROP TABLE IF EXISTS `sell`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sell` (
+  `auctionID` int NOT NULL,
+  `itemID` int NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `init_price` decimal(10,2) DEFAULT NULL,
+  `bid_increment` decimal(10,2) DEFAULT NULL,
+  `min_price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`auctionID`,`itemID`,`username`),
+  KEY `itemID` (`itemID`),
+  KEY `username` (`username`),
+  CONSTRAINT `sell_ibfk_1` FOREIGN KEY (`auctionID`) REFERENCES `auction` (`auctionID`),
+  CONSTRAINT `sell_ibfk_2` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`),
+  CONSTRAINT `sell_ibfk_3` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sell`
+--
+
+LOCK TABLES `sell` WRITE;
+/*!40000 ALTER TABLE `sell` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sell` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shirt`
+--
+
+DROP TABLE IF EXISTS `shirt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shirt` (
+  `itemID` int NOT NULL,
+  `shirt_size` char(1) DEFAULT NULL,
+  `style` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`itemID`),
+  CONSTRAINT `shirt_ibfk_1` FOREIGN KEY (`itemID`) REFERENCES `clothing` (`itemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shirt`
+--
+
+LOCK TABLES `shirt` WRITE;
+/*!40000 ALTER TABLE `shirt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shirt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -25,12 +316,12 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int NOT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL,
+  `username` varchar(30) NOT NULL,
   `password` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
   `type` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,9 +331,27 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (0,'John Doe','admin1','password','adminstaff@buyme.com','600 William Ave. Neenah, WI 54956','admin'),(1,'Crystal Smith','customerrep1','password','csmith@buyme.com','7311 E. Fieldstone Dr. Des Moines, IA 50310','rep'),(2,'Billy Bob','customerrep2','password','bbob@buyme.com','390 Border Street Anaheim, CA 92806','rep'),(3,'Judy Mae','customerrep3','password','jmae@buyme.com','7486 Schoolhouse Ave. Hamtramck, MI 48212','rep'),(4,'Mary Williams','customerrep4','password','mwilliams@buyme.com','8628 Sugar Street Palm Harbor, FL 34683','rep'),(5,'Josh Stewart','customerrep5','password','jstewart@buyme.com','12 Grand St. Glenside, PA 19038','rep'),(1006,'annie z','annie1','blahblah','aaa@gmail.com','123 something USA','end'),(1007,'annie','annie','password','a@gmail.com','sfsf','end'),(1008,'annie','aaa','aaa','fasfsa','afs','end');
+INSERT INTO `user` VALUES (0,'John Doe','admin1','password','adminstaff@buyme.com','600 William Ave. Neenah, WI 54956','admin'),(1009,'Annie Zhong','azhong','az12345','azhong@gmail.com','123 Scenic Ln, Kipling, OH','end'),(1,'Crystal Smith','customerrep1','password','csmith@buyme.com','7311 E. Fieldstone Dr. Des Moines, IA 50310','rep'),(2,'Billy Bob','customerrep2','password','bbob@buyme.com','390 Border Street Anaheim, CA 92806','rep'),(3,'Judy Mae','customerrep3','password','jmae@buyme.com','7486 Schoolhouse Ave. Hamtramck, MI 48212','rep'),(4,'Mary Williams','customerrep4','password','mwilliams@buyme.com','8628 Sugar Street Palm Harbor, FL 34683','rep'),(5,'Josh Stewart','customerrep5','password','jstewart@buyme.com','12 Grand St. Glenside, PA 19038','rep'),(1007,'Mary Lou','mlou','password','Mlou@gmail.com','123 rutgers ave','end'),(1006,'Mary','mlou@gmail.com','password','Lou','123 rutgers ave','end'),(1008,'May Smith','ms123','password','msmith@gmail.com','123 rutgers ave','end');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `end_view`
+--
+
+/*!50001 DROP VIEW IF EXISTS `end_view`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `end_view` AS select `c`.`name` AS `name`,`c`.`gender` AS `gender`,`c`.`brand` AS `brand`,`a`.`username` AS `username` from (`auction` `a` join `clothing` `c`) where (`a`.`itemID` = `c`.`itemID`) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +362,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-25 16:45:27
+-- Dump completed on 2021-08-12 11:19:54
